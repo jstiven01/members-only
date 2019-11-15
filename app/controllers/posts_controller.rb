fcfab7bdc77f77aa19fa36d838d2926d333a161a
jstiven01 <stiven10@gmail.com>
2019-11-15 16:44:04 -0500

    Building Posts controller create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,16 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def create; end
+  def create
+    params_permited = post_params
+    @post = Post.new(user_id: session[:user_id], title: params_permited[:title], content: params_permited[:content])
+    if @post.save
+      redirect_to posts_path
+    else
+      flash.now[:danger] = 'Error creating the post'
+      render 'new'
+    end
+  end
 
   def index; end
 
@@ -17,5 +26,9 @@ class PostsController < ApplicationController
 
     flash[:danger] = 'Please log in.'
     redirect_to signin_url
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
