bf90663f1e9f3bb9a775758503c5821c9f810fc4
jstiven01 <stiven10@gmail.com>
2019-11-15 10:51:30 -0500

    Creating static page home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root   'static_pages#home'
   get    '/signin',   to: 'sessions#new'
   post   '/signin',   to: 'sessions#create'
   delete '/signout',  to: 'sessions#destroy'
