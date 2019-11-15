2bcda8b71a8b14ba3a0236208cb53ee11c43e233
jstiven01 <stiven10@gmail.com>
2019-11-15 13:19:53 -0500

    Creating Post model Posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
   root   'static_pages#home'
   get    '/signin',   to: 'sessions#new'
   post   '/signin',   to: 'sessions#create'
