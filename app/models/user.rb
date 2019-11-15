9061393ca3896594ffa3692f1b15534372ac3bfc
jstiven01 <stiven10@gmail.com>
2019-11-14 19:04:32 -0500

    Creating remember token and populating db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
+  before_create :create_remember_token
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
@@ -8,4 +11,24 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  class << self
+    # Returns the hash digest of the given string.
+    def digest(string)
+      Digest::SHA1.hexdigest(string.to_s)
+    end
+
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  private
+
+  def create_remember_token
+    self.remember_token = User.new_token
+    self.remember_digest = User.digest(remember_token)
+  end
 end
